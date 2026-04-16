432f2847b792c719adfb50f6553b66adbbf341be
sanjana631 <sanjanadass631@gmail.com>
2026-04-16 12:00:02 +0530

    Add Main class with addition functionality

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,11 @@
-
+public class Main {
+    public static void main(String[] args) {
+        int firstNum = 10;
+        int secondNum = 20;
+        
+        // The '+' operator performs the addition
+        int sum = firstNum + secondNum;
+        
+        System.out.println("The sum is: " + sum);
+    }
+}
